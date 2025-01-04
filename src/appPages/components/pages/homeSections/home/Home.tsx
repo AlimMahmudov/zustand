0bf903bdb11f6ab50f1732c402e5f985824f14e6
Alim Mahmudov <alimmahmudov27@gmail.com>
2025-01-04 17:44:32 +0600

    Home

--- src/appPages/components/pages/homeSections/home/Home.tsx
+++ src/appPages/components/pages/homeSections/home/Home.tsx
@@ -1,6 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
 import scss from "./Home.module.scss";
+import zustand from "@/stores/zustand";
 
 const Home = () => {
+  const [image, setImage] = useState("");
+  const [title, setTitle] = useState("");
+  const [imageEdit, setImageEdit] = useState("");
+  const [titleEdit, setTitleEdit] = useState("");
+  const [isEdit, setIsEdit] = useState<number | null>(null);
+  const { GetProduct, PostProduct, DeleteProduct, PatchProduct, product } =
+    zustand();
+
+  const onSubmit = async () => {
+    const newProduct = {
+      image,
+      title,
+    };
+    await PostProduct(newProduct);
+    setImage("");
+    setTitle("");
+  };
+
+  const onSubmitEdit = async () => {
+    if (isEdit !== null) {
+      const newProduct = {
+        image: imageEdit,
+        title: titleEdit,
+      };
+      await PatchProduct(isEdit, newProduct);
+      setIsEdit(null);
+      setImageEdit("");
+      setTitleEdit("");
+    }
+  };
+
+  useEffect(() => {
+    GetProduct();
+  }, [GetProduct]);
+
   return (
     <div id={scss.Home}>
       <div className="container">
